04f0ed927c3e2534060a374b1954b338a46a5976
exceleapp <exceleapp@gmail.com>
2019-07-10 09:51:17 +0700

    +++

--- application/models/Target_SKP_model.php
+++ application/models/Target_SKP_model.php
@@ -13,10 +13,10 @@ class Target_SKP_model extends CI_Model
     public function getSKP($id_skp)
     {
         $this->db->select('nip,kode_jabatan,uraian,output,satuan_output,mutu,waktu,satuan_waktu,biaya');
-		$this->db->from('skp_dataskp');
+		// $this->db->from('skp_dataskp');
         $this->db->join('skp_t_kerja', 'skp_t_kerja.id_skp = skp_dataskp.id_skp ');
         // $this->db->join('log_masteraktivitas', ' log_masteraktivitas.bk_id= log_aktivitas.akt_idkegiatan');
-       return $this->db->get_where('skp_datskp',['id_skp' => $id_skp])->result_array();
+       return $this->db->get_where('skp_dataskp',['skp_dataskp.id_skp' => $id_skp])->result_array();
        
     }
 }
