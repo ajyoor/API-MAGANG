988853e115bd772c562935734e9cea5d9918ae0a
Excel Anggara <exceleapp@gmail.com>
2019-07-22 14:07:55 +0700

    ==

--- application/controllers/api/Login_SKP.php
+++ application/controllers/api/Login_SKP.php
@@ -1,11 +1,19 @@
 <?php
-class Login_SKP extends CI_Controller
-{
+use Restserver\Libraries\REST_Controller;
+defined('BASEPATH') OR exit('No direct script access allowed');
+require APPPATH . 'libraries/REST_Controller.php';
+require APPPATH . 'libraries/Format.php';
+ 
+class Login_SKP extends CI_Controller{
+    use REST_Controller {
+    REST_Controller::__construct as private __resTraitConstruct;
+
+}
     public function __construct()
 {
-parent::__construct();
-$this->load->model('Login_SKP_model');
-$this->load->helper('text');
+    parent::__construct();
+    $this->__resTraitConstruct();
+    $this->load->model('Login_SKP_model','login');
 }
 
 // code for login *api*-------------------------------
@@ -13,7 +21,15 @@ $this->load->helper('text');
 public function login_api_ci(){
 $username =   $this->input->get('username');
 $pass =    $this->input->get('password');
-$query = $this->Login_SKP_model->login_api($username,$pass);
+$query = $this->login->login_api($username,$pass);
+if ($query !== null) {
+    $this->response([
+        'status' => true,
+        'data'   => $query,
+        'message'=> 'Assalamualaikum'
+    ], 200);
+}
+
 echo json_encode($query);
 }
 }
