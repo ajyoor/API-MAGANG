d1ec7446873ce13e0a28b661a37000c8a3de86a9
Excel Anggara <exceleapp@gmail.com>
2019-07-30 09:45:05 +0700

    Profil

--- application/models/Profil_model.php
+++ application/models/Profil_model.php
@@ -12,9 +12,9 @@ class Profil_model extends CI_Model
     public function getProfil($nip)
     {
         if( $nip != null)   {
-            $this->db->select("DISTINCT sp.image_profil,sp.nama,sj.nama_jabatan,sp.nip,CONCAT(sp.tempat_lahir,', ', sp.tanggal_lahir) as TTL,sp.notelp,sp.email,sp.alamat,sg.nama_golongan,lpa.kodeja_penilai,lpa.nip_penilai", FALSE);
+            $this->db->select("DISTINCT sp.image_profil,sp.nama,sj.nama_jabatan,sp.nip,CONCAT(sp.tempat_lahir,', ', sp.tanggal_lahir) as TTL,sp.notelp,sp.email,sp.alamat,sg.nama_golongan,(select nama_jabatan from skp_jabatan where kode_jabatan=lpa.kodeja_penilai) as Jabatan_Penilai,(select nama from skp_pns where nip=lpa.nip_penilai) as nama_penilai", FALSE);
             $this->db->from("skp_pns sp");
-            $this->db->join("log_penilaiaktivitas lpa", "lpa.nip_penilai = sp.nip");
+            $this->db->join("log_penilaiaktivitas lpa", "lpa.nip = sp.nip");
             $this->db->join("skp_jabatan sj", "sj.kode_jabatan = sp.kode_jabatan ");
             $this->db->join("skp_golongan sg", "sg.id_gol= sp.id_golongan");
             $this->db->where("sp.nip",$nip);
